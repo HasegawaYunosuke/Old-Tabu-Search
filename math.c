2acb55e30b5baad86fea21c6a8ba0024d81374c9
Hasegawa Yunosuke <hasegawa.yunosuke@gmail.com>
2009-10-13 03:46:32 +0900

    add randam procedure for 'create initial path'
    in "math.c". But I have to more randamized areange.

--- math.c
+++ math.c
@@ -3,8 +3,21 @@
 
 /* functions */
 double make_distance(int x1, int y1, int x2, int y2);
+int random_num(int maximum);
 
 double make_distance(int x1, int y1, int x2, int y2)
 {
     return sqrt(pow((x1 - x2), 2.0) + pow((y1 - y2), 2.0));
+}
+
+int random_num(int maximum)
+{
+    int return_data;
+
+    /* (1 <= return_data <= maximum) */
+    if((return_data = rand() % maximum) == 0) {
+        return_data = maximum;
+    }
+
+    return return_data;
 }
