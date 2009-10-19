d88b9e511b8108881421bf6b94a7ae9eaa469167
Hasegawa Yunosuke <hasegawa.yunosuke@gmail.com>
2009-10-19 16:40:12 +0900

    ほんとすいません
    tozaki.cもaddしてませんでした。

---
+++ tozaki.c
@@ -0,0 +1,13 @@
+/*
+    This file "tozaki.c" can edit by ONLY Tozaki.
+*/
+
+/* header files */
+#include "header.h"
+
+/* functions */
+int * tozaki_search(int * solution_path);
+
+int * tozaki_search(int * solution_path)
+{
+}
