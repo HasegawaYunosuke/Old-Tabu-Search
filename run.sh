b2ccc49231bf9648d937c634ca7d73c8b4a618c6
share <Al-Lab@cm.kansai-u.ac.jp>
2009-12-16 01:49:25 +0900

    pull用

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ln -s /home/share/machinefile machinefile
-scrun -group=machinefile,network=sctp ./mpi.exe -f tsp_data/rat575.tsp -b -m -t 3
+scrun -group=machinefile,network=sctp ./mpi.exe -f tsp_data/rat575.tsp -l -m -t 300
