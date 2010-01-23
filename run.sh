fa4691299e0901cb0a5b3d756e5042529a3a062d
share <Al-Lab@cm.kansai-u.ac.jp>
2010-01-24 00:15:57 +0900

    出力ログファイルからゴミを除去

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 ln -s /home/share/machinefile machinefile
 #scrun -group=pcc,network=sctp -nodes=2x2 ./mpi.exe -f tsp_data/rat575.tsp -b -m -t 30 -v 2
-scrun -group=pcc,network=sctp -nodes=8x2 ./mpi.exe -f tsp_data/eil51.tsp -b -m -t 120
+scrun -group=pcc,network=sctp -nodes=8x2 ./mpi.exe -f tsp_data/eil51.tsp -b -m -t 300
