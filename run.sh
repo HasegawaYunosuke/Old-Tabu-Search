3cb73902223fe4b45d5cbbd0248223d524639cff
share <Al-Lab@cm.kansai-u.ac.jp>
2010-01-24 02:23:25 +0900

    微修正

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ln -s  /home/share/pole/tsp.repo machinefile
-scrun -group=pcc,network=sctp -nodes=8x2 ./mpi.exe -f tsp_data/eil51.tsp -l -m -t 120
+scrun -group=pcc,network=sctp -nodes=8x2 ./mpi.exe -f tsp_data/eil51.tsp -b -m -t 300
