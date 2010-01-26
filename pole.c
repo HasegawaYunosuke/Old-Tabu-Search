ba70c7ad2b4a8fc0b7c97edc99171cad66ff8d1e
Tanaka Yuya <twilight_star@hotmail.co.jp>
2010-01-26 23:11:55 +0900

    other_solution_pathの利用部分修正

--- pole.c
+++ pole.c
@@ -121,30 +121,31 @@ void transform_solution_path(int * other_solution_path, int * return_path)
     int tsp_size = get_tsp_size();
     int i;
     int *path_a;
-    int *path_b;
-    int *path_c;
+    //int *path_b;
+    //int *path_c;
 
     path_a = mallocer_ip(tsp_size + 1);
-    path_b = mallocer_ip(tsp_size + 1);
-    path_c = mallocer_ip(tsp_size + 1);
+    //path_b = mallocer_ip(tsp_size + 1);
+    //path_c = mallocer_ip(tsp_size + 1);
 
     path_a[0] = tsp_size;
-    path_b[0] = tsp_size;
-    path_c[0] = tsp_size;
+    //path_b[0] = tsp_size;
+    //path_c[0] = tsp_size;
 
     for(i = 0; i <= tsp_size; i++){
         path_a[i + 1] = other_solution_path[i];
-        path_b[i + 1] = other_solution_path[i + DEFAULT_SENDPARAMETERNUM + tsp_size];
-        path_c[i + 1] = other_solution_path[i + (DEFAULT_SENDPARAMETERNUM + tsp_size) * 2];
+        //path_b[i + 1] = other_solution_path[i + DEFAULT_SENDPARAMETERNUM + tsp_size];
+       // path_c[i + 1] = other_solution_path[i + (DEFAULT_SENDPARAMETERNUM + tsp_size) * 2];
        }
 
-    int num = rand() % 3;
+    //int num = rand() % 3;
 
-    if(num == 0){
+    //if(num == 0){
         for(i = 0; i < tsp_size + 1; i++){
             return_path[i] = path_a[i];
-        }
-    }
+            }
+   // }
+   /*
     if(num == 1){
         for(i = 0; i < tsp_size + 1; i++){
             return_path[i] = path_b[i];
@@ -154,11 +155,11 @@ void transform_solution_path(int * other_solution_path, int * return_path)
         for(i = 0; i < tsp_size + 1; i++){
             return_path[i] = path_c[i];
             }
-    }
+    }*/
 
     free(path_a);
-    free(path_b);
-    free(path_c);
+    //free(path_b);
+    //free(path_c);
 }
 
 /*two opt only*/
