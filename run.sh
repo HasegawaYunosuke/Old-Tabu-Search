5aadc221c2092c6ad4a122e00fcd4bcbabbfb1c7
share <Al-Lab@cm.kansai-u.ac.jp>
2010-02-02 17:45:22 +0900

    a bit changed

--- run.sh
+++ run.sh
@@ -22,8 +22,7 @@ if [ $1 == 5 ]
 then
 scrun -group=machinefile,network=sctp -nodes=4x2 ./nompi.exe -f tsp_data/rat575.tsp -b -t 30
 fi
-if [ $1 == 6 ]
+if [ $1 == 3 ]
 then
-#scrun -group=machinefile,network=sctp -nodes=4x2 ./mpi.exe -f tsp_data/rat575.tsp -b -m -t 60
-scrun -group=pcc,network=sctp -nodes=8x2 ./mpi.exe -f tsp_data/rat575.tsp -b -m -t 1800
+scrun -group=machinefile2,network=sctp -nodes=4x2 ./mpi.exe -f tsp_data/rat575.tsp -b -m -t 1800
 fi
